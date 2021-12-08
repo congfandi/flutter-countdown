a3147adeb8f84b8de057a99ff93f94a1d2e350ec
Cong Fandi <congfandi@gmail.com>
2021-12-08 10:35:05 +0700

    fixing migrate to null safety

--- android/src/main/java/com/example/countdown_flutter/CountdownFlutterPlugin.java
+++ android/src/main/java/com/example/countdown_flutter/CountdownFlutterPlugin.java
@@ -1,11 +1,10 @@
 package com.example.countdown_flutter;
 
-import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 /** CountdownFlutterPlugin */
 public class CountdownFlutterPlugin implements MethodCallHandler {
   /** Plugin registration. */
